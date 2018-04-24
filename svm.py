6eb502219e7ae3889086f68281a2e8b6a28f70e7
IsaacShrestha <ishrestha@unomaha.edu>
2018-04-24 01:38:12 -0500

    generated SVM model

--- svm.py
+++ svm.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 23 21:14:51 2018
+Created on Tue Apr 24 01:16:14 2018
 
 @author: isaacshrestha
 """
-# Logistic Regression
 
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd 
+# Support Vector Machine
 
-# Importing the dataset
 dataset = pd.read_excel('dataset.xlsx')
 X = dataset.iloc[:, [4,5,6,9,10]].values
 y = dataset.iloc[:, 12].values
@@ -46,24 +41,7 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fitting Logistic Regression to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making confusion matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# Calculating accuracy 
-from sklearn.metrics import accuracy_score
-accuracy_score(y_test, y_pred)
-
-# Calculating average precision score
-from sklearn.metrics import average_precision_score
-average_precision = average_precision_score(y_test, y_pred)
-
-
