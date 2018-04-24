1400671d767a4a4d52968589948820b97869bdbf
IsaacShrestha <ishrestha@unomaha.edu>
2018-04-23 21:21:02 -0500

    dataset and beginning of logistic regression

---
+++ logistic-regression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 21:14:51 2018
+
+@author: isaacshrestha
+"""
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
